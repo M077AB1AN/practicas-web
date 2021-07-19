596c301fbfbfcacb965edfe9a60bb644d0c95251
M077AB1AN <82134219+M077AB1AN@users.noreply.github.com>
2021-07-19 03:56:41 -0500

    Add files via upload

---
+++ LABORATORIO 5/JS/bibliotecaemergente.js
@@ -0,0 +1,5 @@
+var x=(anc/2)-(ancho/2); var y=(alt/2)-(alto/2); 
+// alert(x); // alert(y); 
+window.open(uri, titulo, "width="+ancho+", height="+alto+", left="+x+", top="+y+", so rollbars=No"); 
+//llamamos a la funcion cargar 
+window.onload=Cargar("pagina4.html","Ventana Emergente",400,400); 
---
+++ LABORATORIO 5/JS/registro.js
@@ -0,0 +1,14 @@
+var registro=[];
+//creamos un procedimiento para poder Registrar
+function Registrar(nomregistro,aperegistro,corregistro) {
+    var NuevoRegistro={
+        nombre:nomregistro,
+        apellido:aperegistro,
+        correo:corregistro,
+    };
+    registro.push(NuevoRegistro);
+}
+//creamos una funcion para mostrar la informacion del registro
+function Mostrar(){
+    return registro;
+}
